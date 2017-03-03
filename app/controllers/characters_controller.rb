ee783a05f6aff912c2964a7ee757416c47132561
schreckda <david.schreck@outlook.com>
2017-03-03 16:04:22 +0100

    new start page and a simple add + show character

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,22 @@
+class CharactersController < ApplicationController
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @character = Character.new(character_params)
+
+    @character.save
+    redirect_to @character
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :race)
+  end
+
+end
