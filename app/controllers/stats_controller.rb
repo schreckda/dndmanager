83cd22b5e34ff7160f8bbfb9bb73113b528f43dd
schreckda <david.schreck@outlook.com>
2017-03-06 23:53:18 +0100

    stats controllers

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,13 @@
 class StatsController < ApplicationController
+
+  def create
+    character = Character.find(params[:character_id])
+    stat = character.stats.create(stat_params)
+    redirect_to character_path(character)
+  end
+
+  private
+  def stat_params
+    params.require(:stat).permit(:strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)
+  end
 end
