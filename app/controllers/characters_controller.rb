b7822d7e73a25578ca7aaca946978864feeb60d4
schreckda <david.schreck@outlook.com>
2017-03-06 22:52:53 +0100

    added edit function

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,13 +12,27 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
+  def edit
+    @character = Character.find(params[:id])
+  end
+
   def create
     @character = Character.new(character_params)
 
-    if character.save
-      redirect to @character
+    if @character.save
+      redirect_to @character
     else
       render 'new'
+    end
+  end
+
+  def update
+    @character = Character.find(params[:id])
+
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
     end
   end
 
